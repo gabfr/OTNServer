ba6b0cb257e19c4fa65420d2e5543be7814dfe5d
Gabriel Ferreira Rosalino <eu@gabrielf.com>
2012-08-29 21:37:52 -0300

    Problems with checksum

--- src/gameserver.js
+++ src/gameserver.js
@@ -1,24 +1,44 @@
 var net = require('net');
 var NetworkMessage = require('./../lib/networkmessage');
-
-var gamePort = 7172;
+var Adler = require('./../lib/adler');
+var gamePort = 7171;
 function GameServer() {
     this.onDataReceive = (function(self) {
-        return function(mBuf) {
+        return function(mBuf) { // Socket environment.
             // look at: http://nodejs.org/api/buffer.html
             // TODO: implement encryption things
+            console.log('Packet encrypted received!');
+            //var msg = new NetworkMessage(mBuf);
+            
+            // ON LOGIN
             var msg = new NetworkMessage(mBuf);
+            var packSize = msg.DecodeHeader();
+            if (packSize <= 0 || packSize >= msg.NETWORKMESSAGE_MAXSIZE - 16) {
+                console.log('Packet with wrong size! Possibly attack?');
+            }
+            console.log('Packet size: ' + packSize);
+            var recvChecksum = msg.GetU32();
+            var checksum = 0;
+            var recvChecksumLength = msg.MsgSize - msg.ReadPos - 4;
+            if (recvChecksumLength > 0) {
+                checksum = msg.GetChecksum(6);
+            }
+            if (recvChecksum == checksum) {
+                console.log('ok!');
+            } else {
+                console.log('n');
+            }
         };
     })(this);
     
     this.onConnect = (function(self) {
-        return function() {
+        return function() { // Socket environment.
             console.log('First byte.');
         };
     })(this);
     
     this.onEnd = (function(self) {
-        return function() {
+        return function() { // Socket environment.
             console.log('Connection closed.');
         };
     })(this);
@@ -35,6 +55,8 @@ function GameServer() {
     this.Listen = (function(self) {
         return function() {
             this.on('connection', self.ConnectionHandler);
+            var addr = this.address();
+            console.log('Game Server listening on: ' + addr.address + ':' + addr.port);
         };
     })(this);
 }
@@ -42,4 +64,4 @@ function GameServer() {
 var mGame = new GameServer();
 
 var gameServer = net.createServer();
-gameServer.listen({port: gamePort}, mGame.Listen);
+gameServer.listen(gamePort, mGame.Listen);
